843220c4930edb38165043cb8f82ea55546f0365
vladbusov <to2002to2002@gmail.com>
2019-11-04 13:16:24 +0300

    fix problems on django 2.2

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from blog.managers import QuestionManager, QuestionVoteManager, UserManager, TagManager
+from blog.managers import QuestionManager, UserManager, TagManager
+
 
 class UserProfile(models.Model):
-    avatar = models.ImageField(null=True, blank=True, verbose_name=u"аватар",upload_to='static/images/')
+    avatar = models.ImageField(null=True, blank=True, verbose_name=u"аватар", upload_to='static/images/')
     register_date = models.DateField(null=False, blank=True, auto_now_add=True, verbose_name=u"дата регистрации")
     rating = models.IntegerField(blank=True, default=0, verbose_name=u"рейтинг")
-    user = models.OneToOneField(User, related_name='userprofile', null=False, verbose_name="user")
+    user = models.OneToOneField(User, related_name='userprofile', null=False, verbose_name="user",
+                                on_delete=models.DO_NOTHING)
     objects = UserManager()
+
     def __unicode__(self):
         return self.user.username
 
@@ -16,52 +19,58 @@ class UserProfile(models.Model):
         verbose_name_plural = u'пользователи'
 
 
-
 class Question(models.Model):
-	title = models.CharField(max_length=255, verbose_name="заголовок")
-	text = models.TextField(verbose_name="текст")
-	author = models.ForeignKey(User, verbose_name="автор")
-	tags = models.ManyToManyField("Tag")
-	rating_num = models.IntegerField(verbose_name='рейтинг', default=0)
-	added_on = models.DateTimeField(verbose_name='дата и время добавления', auto_now_add=True)
-	objects = QuestionManager()
+    title = models.CharField(max_length=255, verbose_name="заголовок")
+    text = models.TextField(verbose_name="текст")
+    author = models.ForeignKey(User, verbose_name="автор", on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField("Tag")
+    rating_num = models.IntegerField(verbose_name='рейтинг', default=0)
+    added_on = models.DateTimeField(verbose_name='дата и время добавления', auto_now_add=True)
+    objects = QuestionManager()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	class Meta:
-		verbose_name = 'вопрос'
-		verbose_name_plural = 'вопросы'
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
 
 class Answer(models.Model):
-	content = models.TextField(verbose_name='текст ответа')
-	author = models.ForeignKey(User, verbose_name='автор', on_delete=models.CASCADE)
-	is_correct = models.BooleanField(verbose_name='верный?', default=False)
-	rating_num = models.IntegerField(verbose_name='рейтинг', default=0)
-	added_on = models.DateTimeField(blank=True, auto_now_add=True, verbose_name='дата и время добавления')
-	question = models.ForeignKey(Question, related_name='answers', null=False, verbose_name="вопрос")
-	def __str__(self):
-		return 'Comment №{number} by {user}'.format(number=self.pk, user='Username')
+    content = models.TextField(verbose_name='текст ответа')
+    author = models.ForeignKey(User, verbose_name='автор', on_delete=models.CASCADE)
+    is_correct = models.BooleanField(verbose_name='верный?', default=False)
+    rating_num = models.IntegerField(verbose_name='рейтинг', default=0)
+    added_on = models.DateTimeField(blank=True, auto_now_add=True, verbose_name='дата и время добавления')
+    question = models.ForeignKey(Question, related_name='answers', null=False, verbose_name="вопрос",
+                                 on_delete=models.DO_NOTHING)
 
-	class Meta:
-		verbose_name = 'ответ'
-		verbose_name_plural = 'ответы'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.answer_text = None
+
+    def __str__(self):
+        return 'Comment №{number} by {user}'.format(number=self.pk, user='Username')
+
+    class Meta:
+        verbose_name = 'ответ'
+        verbose_name_plural = 'ответы'
 
 
 class Tag(models.Model):
-	name = models.CharField(max_length=255, verbose_name="имя")
-	objects = TagManager()
+    name = models.CharField(max_length=255, verbose_name="имя")
+    objects = TagManager()
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = 'тег'
-		verbose_name_plural = 'теги'
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
 
 
 class Like(models.Model):
-    by_user = models.ForeignKey(User, null=False, verbose_name=u"пользователь")
+    by_user = models.ForeignKey(User, null=False, verbose_name=u"пользователь", on_delete=models.DO_NOTHING)
     is_like = models.BooleanField(blank=True, default=True, verbose_name=u"лайк")
 
     def __unicode__(self):
@@ -74,7 +83,7 @@ class Like(models.Model):
 
 
 class QuestionLike(Like):
-    question = models.ForeignKey(Question, null=False, verbose_name="вопрос")
+    question = models.ForeignKey(Question, null=False, verbose_name="вопрос", on_delete=models.DO_NOTHING)
 
     def __unicode__(self):
         return "Лайк пользователя " + self.by_user.username + " на вопрос " + self.question.title
@@ -84,7 +93,7 @@ class QuestionLike(Like):
 
 
 class AnswerLike(Like):
-    answer = models.ForeignKey(Answer, null=False, verbose_name="ответ")
+    answer = models.ForeignKey(Answer, null=False, verbose_name="ответ", on_delete=models.DO_NOTHING)
 
     def __unicode__(self):
         return "Лайк пользователя " + self.by_user.username + " на ответ " + self.answer.answer_text
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.conf.urls import url
 from django.urls import reverse
 from django.views.generic import TemplateView
-from django.http import HttpResponse, Http404, HttpResponseBadRequest,  JsonResponse, HttpResponseRedirect
-from .models import Question, Answer, Tag,  QuestionLike, AnswerLike
+from django.http import HttpResponse, Http404, HttpResponseBadRequest, JsonResponse, HttpResponseRedirect
+from .models import Question, Answer, Tag, QuestionLike, AnswerLike
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
@@ -27,7 +27,8 @@ for i in range(1, 30):
         'text': 'text' + str(i),
     })
 
-def paginate(request , qs):
+
+def paginate(request, qs):
     try:
         limit = int(request.GET.get('limit', 4))
     except ValueError:
@@ -53,13 +54,14 @@ def main(request):
     success = request.GET.get('continue')
     page = paginate(request, Question.objects.all())
     user = {}
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         user['first_name'] = request.user.first_name
         user['last_name'] = request.user.last_name
-    return render(request, 'index.html', {'posts': page.object_list ,
-                                          'paginator': page.paginator, 'page': page, 'success': success, 'user':user})
+    return render(request, 'index.html', {'posts': page.object_list,
+                                          'paginator': page.paginator, 'page': page, 'success': success, 'user': user})
 
-def question(request, quest_num = 1):
+
+def question(request, quest_num=1):
     scroll = None
     if quest_num == None:
         raise Http404("No questions provided")
@@ -72,34 +74,43 @@ def question(request, quest_num = 1):
     form = AnswerForm()
     page = paginate(request, q.answers.all())
     user_name = None
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         user_name = request.user.first_name
     page.paginator.baseurl = '/question/' + str(quest_num) + '/?page='
     if scroll == None:
-        return render(request, 'pageOfOneQuestion.html', {'posts': page.object_list ,
-                                          'paginator': page.paginator, 'page': page, 'id': quest_num, 'question': q, 'form':form, 'user_name': user_name })
-    if scroll == True:
-        return render(request, 'pageOfOneQuestion.html', {'posts': page.paginator.page(page.paginator.num_pages).object_list,
-                                                          'paginator': page.paginator, 'page': page.paginator.page(page.paginator.num_pages), 'id': quest_num,
+        return render(request, 'pageOfOneQuestion.html', {'posts': page.object_list,
+                                                          'paginator': page.paginator, 'page': page, 'id': quest_num,
                                                           'question': q, 'form': form, 'user_name': user_name})
+    if scroll == True:
+        return render(request, 'pageOfOneQuestion.html',
+                      {'posts': page.paginator.page(page.paginator.num_pages).object_list,
+                       'paginator': page.paginator, 'page': page.paginator.page(page.paginator.num_pages),
+                       'id': quest_num,
+                       'question': q, 'form': form, 'user_name': user_name})
+
 
 def hot(request):
-    return render(request, 'thistag.html' )
+    return render(request, 'thistag.html')
+
 
 def ask(request):
     return render(request, 'newquestion.html')
 
+
 def signup(request):
     return render(request, 'registration.html')
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def tag(request, tag):
-    return render(request, 'tag.html', {'tag': tag ,})
+    return render(request, 'tag.html', {'tag': tag, })
+
 
 def settings(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         if request.method == "POST":
             first_name = request.POST.get("first_name")
             last_name = request.POST.get("last_name")
@@ -122,20 +133,23 @@ def settings(request):
                 auth.login(request, user)
             return HttpResponseRedirect('/?continue=saveset')
         form = UserRegistrationForm()
-        return render(request, 'settings.html', {'form':form})
+        return render(request, 'settings.html', {'form': form})
     return HttpResponseRedirect('/?continue=notlogin')
+
 
 def questions_hot(request):
     page = paginate(request, Question.objects.hot())
     page.paginator.baseurl = "questions_hot/?page="
-    return render(request, "questions_hot.html", {'posts': page.object_list ,
-                                          'paginator': page.paginator, 'page': page,})
+    return render(request, "questions_hot.html", {'posts': page.object_list,
+                                                  'paginator': page.paginator, 'page': page, })
+
 
 def questions_new(request):
     page = paginate(request, Question.objects.new())
     page.paginator.baseurl = "questions_new/?page="
-    return render(request, "questions_new.html", {'posts': page.object_list ,
-                                          'paginator': page.paginator, 'page': page,})
+    return render(request, "questions_new.html", {'posts': page.object_list,
+                                                  'paginator': page.paginator, 'page': page, })
+
 
 def questions_tag(request, tag):
     if tag == None:
@@ -146,17 +160,18 @@ def questions_tag(request, tag):
         raise Http404("No tag provided")
 
     page.paginator.baseurl = '/tag/' + tag + '/?page='
-    return render(request, "questions_tag.html", {'posts': page.object_list ,
-                                          'paginator': page.paginator, 'page': page, 'tag': tag})
+    return render(request, "questions_tag.html", {'posts': page.object_list,
+                                                  'paginator': page.paginator, 'page': page, 'tag': tag})
 
 
 def logout(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         auth.logout(request)
     return HttpResponseRedirect('/?continue=logout')
 
+
 def make_login(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return HttpResponseRedirect('/?continue=relog')
     if request.method == "POST":
         login = request.POST.get('login')
@@ -169,11 +184,12 @@ def make_login(request):
     else:
         error = request.GET.get('error')
         form = LoginForm()
-    return render(request, 'login.html', {'form': form, 'error': error })
+    return render(request, 'login.html', {'form': form, 'error': error})
+
 
 def registration(request):
     error_fields = []
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return HttpResponseRedirect('/?continue=relog')
     if request.method == "POST":
         first_name = request.POST.get("first_name")
@@ -210,10 +226,11 @@ def registration(request):
 
         if not re.compile("^([A-Za-z0-9]+)+$").match(login_user):
             error_fields.append("Неверный формат логина")
+        user = None
         try:
             user = User.objects.create_user(username=login_user,
-                                       email=email,
-                                       password=password1)
+                                            email=email,
+                                            password=password1)
             user.first_name = first_name
             user.last_name = last_name
             user.save()
@@ -228,8 +245,9 @@ def registration(request):
     form = UserRegistrationForm()
     return render(request, 'registration.html', {'form': form, 'errors': error_fields})
 
+
 def ask_quest(request):
-    if not request.user.is_authenticated():
+    if not request.user.is_authenticated:
         return JsonResponse({'status': 'error',
                              'message': 'Ошибка доступа'})
     if request.method == "POST":
@@ -238,8 +256,8 @@ def ask_quest(request):
         tags = request.POST.get("tags")
 
         qst = Question.objects.create(title=title,
-                                          text=text,
-                                          author=request.user)
+                                      text=text,
+                                      author=request.user)
         tags = tags.split(",")
         for tag in tags:
             tag = (str(tag)).replace(' ', '')
@@ -248,7 +266,7 @@ def ask_quest(request):
         return HttpResponseRedirect('/?page=1000000000')
 
     form = AskForm()
-    return render(request, 'newquestion.html', {'form':form, })
+    return render(request, 'newquestion.html', {'form': form, })
 
 
 @csrf_exempt
@@ -257,7 +275,7 @@ def app_like(request):
     id = request.POST.get("id", None)
     is_like = request.POST.get("is_like", True)
     rating = 0
-    if not request.user.is_authenticated():
+    if not request.user.is_authenticated:
         return JsonResponse({'status': 'error',
                              'message': 'Эта операция доступна только авторизованным пользователям'})
     if not id:
